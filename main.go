fec6b117f326d7a1aaf4a0de26a40ccfba034bf3
Vadym Mamchur <vadym.mamchur.job@gmail.com>
2025-02-10 10:54:40 +0200

    feat: add login command

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"log"
+	"os"
 
 	"github.com/vmamchur/go_blog-aggregator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
 func main() {
 	cfg, err := config.Read()
-    if err != nil {
-        log.Fatalf("Error reading config: %v", err)
-    }
-    fmt.Printf("Read config: %+v\n", cfg)
+	if err != nil {
+		log.Fatalf("Error reading config: %v", err)
+	}
 
-    err = cfg.SetUser("bugsworld")
+	stateInstance := &state{cfg: &cfg}
 
-    cfg, err = config.Read()
-    if err != nil {
-        log.Fatalf("Error reading config: %v", err)
-    }
-    fmt.Printf("Read config again: %+v\n", cfg)
+	cmds := commands{
+		handlers: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+		return
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(stateInstance, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
