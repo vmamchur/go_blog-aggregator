0f6607eaddf145f8badd9ed041d209b7c0e9b9e5
Vadym Mamchur <vadym.mamchur.job@gmail.com>
2025-02-14 19:49:30 +0200

    feat: add command to list users

--- handler_user.go
+++ handler_user.go
@@ -49,6 +49,23 @@ func handlerRegister(s *state, cmd command) error {
 	return nil
 }
 
+func handlerListUsers(s *state, cmd command) error {
+    users, err := s.db.GetUsers(context.Background())
+    if err != nil {
+        return fmt.Errorf("Couldn't list users: %w", err)
+    }
+
+    for _, user := range users {
+        if user.Name == s.cfg.CurrentUserName {
+            fmt.Printf("* %v (current)\n", user.Name)
+            continue
+        }
+        fmt.Printf("* %v\n", user.Name)
+    }
+
+    return nil
+}
+
 func printUser(user database.User) {
 	fmt.Printf(" * ID:    %v\n", user.ID)
 	fmt.Printf(" * Name:  %v\n", user.Name)
--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func main() {
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
+    cmds.register("users", handlerListUsers)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
